1e20e53f972444f9cdf710c48b20148ff3e7e7ba
Adam Abdulkadir <adama1862@outlook.com>
2022-11-07 20:33:24 -0500

    adding postgres to docker

--- server/server.js
+++ server/server.js
@@ -13,16 +13,23 @@ const image = require("./controllers/image");
 const db = knex({
   // connect to your own database here:
   client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "",
-    database: "smart-brain",
-  },
+  connection: process.env.POSTGRES_URI,
 });
 const app = express();
 
+const whitelist = ["http://localhost:3001"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(morgan("combined"));
+app.use(cors(corsOptions));
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get("/", (req, res) => {
