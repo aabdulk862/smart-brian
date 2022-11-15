2001d15793e3296ab08e4330a2063e112dbc43d4
Adam Abdulkadir <adama1862@outlook.com>
2022-11-15 18:31:37 -0500

    authorization

--- server/controllers/signin.js
+++ server/controllers/signin.js
@@ -1,78 +1,78 @@
-const jwt = require("jsonwebtoken");
-const redis = require("redis");
+const jwt = require('jsonwebtoken');
 
-/// Setup Redis
-(async () => {
-  const redisClient = redis.createClient({
-    url: process.env.REDIS_URI,
-  });
+// Redis Setup
+const redis = require('redis');
+// You will want to update your host to the proper address in production
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URI,
+});
 
-  redisClient.on("error", console.error);
+// New for Redis v4+
+// Read the migration guide here: https://github.com/redis/node-redis/blob/HEAD/docs/v3-to-v4.md
+async function redisConnect() {
+ return await redisClient.connect();
+}
+redisConnect()
 
-  await redisClient.connect();
+const signToken = (username) => {
+  const jwtPayload = { username };
+  return jwt.sign(jwtPayload,  process.env.JWT_SECRET, { expiresIn: '2 days'});
+};
 
-  await redisClient.set("key", "value");
-  const value = await redisClient.get("key");
+const setToken = (key, value) => Promise.resolve(redisClient.set(key, value));
 
-  console.log(value);
-})();
+const createSession = (user) => {
+  const { email, id } = user;
+  const token = signToken(email);
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token, user }
+    })
+    .catch(console.log);
+};
 
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return Promise.reject("incorrect form submission");
+    return Promise.reject('incorrect form submission');
   }
-
-  return db
-    .select("email", "hash")
-    .from("login")
-    .where("email", "=", email)
-    .then((data) => {
+  return db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", email)
-          .then((user) => user[0])
-          .catch((err) => Promise.reject("unable to get user"));
+        return db.select('*').from('users')
+          .where('email', '=', email)
+          .then(user => user[0])
+          .catch(err => res.status(400).json('unable to get user'))
       } else {
-        Promise.reject("wrong credentials");
+        return Promise.reject('wrong credentials');
       }
     })
-    .catch((err) => res.status(400).json("wrong credentials"));
-};
+    .catch(err => err)
+}
 
-const getAuthtokenId = () => {
-  console.log("auth ok");
-};
-
-const signToken = (email) => {
-  const jwtPayload = { email };
-  return jwt.sign(jwtPayload, process.env.JWT_SECRET), { expiresIn: "2 days" };
-};
-
-const createSessions = (user) => {
-  // JWT token, return user data
-  const { email, id } = user;
-  const token = signToken(email);
-  return { success: "true", userId: id, token };
-};
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(401).send('Unauthorized');
+    }
+    return res.json({id: reply})
+  });
+}
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
-  return authorization
-    ? getAuthtokenId()
+  return authorization ? getAuthTokenId(req, res)
     : handleSignin(db, bcrypt, req, res)
-        .then((data) => {
-          return data.id && data.email
-            ? createSessions(data)
-            : Promise.reject(data);
-        })
-        .then((session) => res.json(session))
-        .catch((err) => res.status(400).json(err));
-};
+    .then(data =>
+      data.id && data.email ? createSession(data) : Promise.reject(data))
+    .then(session => res.json(session))
+    .catch(err => res.status(400).json(err));
+}
 
 module.exports = {
   signinAuthentication: signinAuthentication,
-};
+  redisClient: redisClient
+}
